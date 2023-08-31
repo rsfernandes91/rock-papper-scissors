8cb747c586117f0645010b2e07ca2f1e05a75d81
Rafael <rsfernandes.contato@gmail.com>
2023-08-31 17:25:42 -0300

    feat: GameChallenge component creation

--- code/src/App.vue
+++ code/src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <GameChallenge />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import GameChallenge from "./components/GameChallenge.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld,
+    GameChallenge,
   },
 });
 </script>
---
+++ code/src/components/GameChallenge.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Teste Novo Component</h1>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "GameChallenge",
+  prop: {},
+});
+</script>
+<style scoped lang="scss"></style>
